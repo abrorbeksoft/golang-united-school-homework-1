13adf5c2e11e77b2d8d5e39060ddd31d6f80aa95
Abrorbek <abrorbek@Abrorbeks-MacBook-Pro.local>
2022-03-06 11:10:01 +0500

    fixed bug

--- solution_test.go
+++ solution_test.go
@@ -9,7 +9,7 @@ func TestGetMessage(t *testing.T) {
 	got := GetMessage()
 	want := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
 
-	if strings.EqualFold(got, want) {
+	if !strings.EqualFold(got, want) {
 		t.Errorf("got %s want %s", got, want)
 	}
 }
